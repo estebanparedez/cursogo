f7ad67adf9023214c24f3d649f54ca39f1bb1366
Esteban <estebanparedez@gmail.com>
2020-05-04 22:51:21 -0300

    commi

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 
 	x, y, z := getMultiplesVariables()
 
+	decimal32, decimal64 := getDecimal()
+
 	fmt.Println("Hola ", name)
 
 	fmt.Printf(holaMundo, name, lastName)
@@ -21,9 +23,30 @@ func main() {
 	fmt.Println("")
 
 	fmt.Println(miNumero, x, y, z)
+
+	fmt.Println(decimal32, decimal64)
 }
 
 func getName() string {
 
 	var name string = "Nombre por defecto"
+
+	fmt.Print("Ingresa tu nombre: ")
+
+	fmt.Scanf("%s", &name)
+
+	return name
+
+}
+
+func getMultiplesVariables() (int, int32, int64) {
+	return 1, 225252525, 252525252242352525
+}
+
+func suma(a int, b int) int {
+	return a + b
+}
+
+func getDecimal() (float32, float64) {
+	return float32(0.1), float64(float32(0.1))
 }
