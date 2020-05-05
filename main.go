23ab660e1085271523490df5d8fd8ff5bb332132
Esteban <estebanparedez@gmail.com>
2020-05-04 22:27:37 -0300

    v2

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
-main.go func() {
+package main
 
-}()
+import "fmt"
+
+const holaMundo string = "Hola %s %s, bienvenido al curso de Go"
+
+func main() {
+
+	name := getName()
+
+	lastName := "Paredez"
+
+	var miNumero = suma(40, 60)
+
+	x, y, z := getMultiplesVariables()
+
+	fmt.Println("Hola ", name)
+
+	fmt.Printf(holaMundo, name, lastName)
+
+	fmt.Println("")
+
+	fmt.Println(miNumero, x, y, z)
+}
+
+func getName() string {
+
+	var name string = "Nombre por defecto"
+}
